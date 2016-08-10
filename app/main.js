fb8fd1c581ee06b80ab2fc11eeaf6a495f3728f8
Earthinferno <graham_small@outlook.com>
2016-08-10 16:01:59 -0700

    Updates

--- app/main.js
+++ app/main.js
@@ -1,7 +1,9 @@
 "use strict";
+var core_1 = require('@angular/core');
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var app_component_1 = require('./app.component');
 var app_routes_1 = require('./app.routes');
+core_1.enableProdMode();
 platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [
     app_routes_1.appRouterProviders
 ]);
